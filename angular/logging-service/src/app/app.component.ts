dd46209993bec9492bd42d49d9b4852a915d76d5
Lorenzo <lorenzozar@gmail.com>
2021-12-17 16:32:23 +0200

    display list of titles using service

--- angular/logging-service/src/app/app.component.ts
+++ angular/logging-service/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from './book.service';
+import { Book } from './models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Log Service';
+export class AppComponent implements OnInit {
+  title = 'Passing Data with Angular Services';
+  booksList: Book[] | undefined;
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.booksList = this.bookService.getBooksList();
+  }
 }
